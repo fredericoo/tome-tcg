97eaa7369f5c178be9e3677f253e091e60147a88
freddie <rincofrederico@gmail.com>
2024-03-15 10:46:25 +0000

    feat: run game via cli

--- apps/tome-api/src/lib/game-engine.ts
+++ apps/tome-api/src/lib/game-engine.ts
@@ -93,12 +93,12 @@ const resolveWinnerField = (
 	throw new Error(`Failed resolving winner field between “${cardA.name}” and “${cardB.name}”`);
 };
 
-export const playGame = async ({
+export const playGame = ({
 	decks,
 	settings,
 }: {
 	decks: Record<Side, DbCard[]>;
-	settings: { castTimeoutMs: 10000 };
+	settings: { castTimeoutMs: number; spellTimeoutMs: number };
 }) => {
 	const finishedTurns: Turn[] = [];
 	const board = initialiseGameBoard({ decks });
@@ -178,8 +178,9 @@ export const playGame = async ({
 				sideB: { submit: castB.submitAction, config: { type: 'any' }, type: 'cast_from_hand' },
 			},
 		};
+		await Promise.all([castA.completed, castB.completed]);
 
-		await Promise.allSettled([castA.completed, castB.completed]);
+		console.log(castA.completed, castB.completed);
 
 		yield { board };
 
@@ -225,83 +226,10 @@ export const playGame = async ({
 					};
 				},
 			},
-			timeoutMs: 10000,
+			timeoutMs: settings.spellTimeoutMs,
 			onTimeout: noop,
 		});
 	}
 
 	return handleTurn();
 };
-
-const _cards: DbCard[] = [
-	{
-		id: '1',
-		type: 'field',
-		name: 'Sacred Pool',
-		description: 'If both players cast spells from the blue stack during combat, heals both wizards for 10 HP.',
-		color: 'blue',
-		effects: {
-			afterCombat: async function* ({ board, turn }) {
-				if (turn.spells.sideA?.slot === 'blue' && turn.spells.sideB?.slot === 'blue') {
-					board.players.sideA.hp += 10;
-					board.players.sideB.hp += 10;
-					yield { board };
-				}
-			},
-		},
-	},
-	{
-		id: '2',
-		type: 'spell',
-		name: 'Frost Burn',
-		description: 'When you cast a spell from the blue stack, discard the top card from your opponents green stack.',
-		colors: ['blue'],
-		attack: 10,
-		effects: {
-			beforeCombat: async function* ({ actions, ownerSide, turn, board }) {
-				if (turn.spells[ownerSide]?.slot === 'blue') {
-					const opponentSide = ownerSide === 'sideA' ? 'sideB' : 'sideA';
-					actions.moveTopCard(board.players[opponentSide].stacks.green, board.players[opponentSide].discardPile);
-					yield { board };
-				}
-			},
-		},
-	},
-	{
-		id: '3',
-		type: 'field',
-		name: 'Void Space',
-		description: 'Before combat, each player chooses one of their spell slots, and discards the top card from it',
-		color: 'neutral',
-		effects: {
-			beforeCombat: async function* ({ actions, board }) {
-				const discardFromStack = (stack: SpellStack, side: Side) => {
-					const selectedStack = board.players[side].stacks[stack];
-					const cardToDiscard = topOf(selectedStack);
-					if (!cardToDiscard) return;
-					actions.discard({ card: cardToDiscard, from: selectedStack, side });
-				};
-
-				for (const side of SIDES) {
-					yield* actions.playerAction({
-						action: {
-							type: 'select_spell_stack',
-							config: {},
-							onAction: ({ stack }) => {
-								discardFromStack(stack, side);
-							},
-						},
-						side,
-						timeoutMs: 10000,
-						onTimeout: () => {
-							const randomStack = STACKS[Math.floor(Math.random() * STACKS.length)];
-							invariant(randomStack, 'randomStack is undefined');
-							discardFromStack(randomStack, side);
-						},
-					});
-				}
-				yield { board };
-			},
-		},
-	},
-];
