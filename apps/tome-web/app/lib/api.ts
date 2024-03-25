995b7e076ed80749650abf958b36fa5c496097c7
freddie <rincofrederico@gmail.com>
2024-03-25 13:27:33 +0000

    feat: use env for client api url

--- apps/tome-web/app/lib/api.ts
+++ apps/tome-web/app/lib/api.ts
@@ -2,6 +2,6 @@ import { edenFetch, treaty } from '@elysiajs/eden';
 
 import type { App } from '../../../tome-api/src/index';
 
-export const fetchFromClient = edenFetch<App>('http://192.168.0.38:8080');
+export const fetchFromClient = edenFetch<App>(import.meta.env.VITE_API_URL);
 
-export const api = treaty<App>('http://192.168.0.38:8080', { fetch: { credentials: 'include' } });
+export const api = treaty<App>(import.meta.env.VITE_API_URL, { fetch: { credentials: 'include' } });
