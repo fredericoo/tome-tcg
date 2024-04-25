f45704e2daeddde4d55ea770f28d2a208495a2bc
freddie <rincofrederico@gmail.com>
2024-04-25 11:06:00 +0100

    feat: cache assets

--- apps/tome-web/app/entry.client.tsx
+++ apps/tome-web/app/entry.client.tsx
@@ -1,12 +1,21 @@
-import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
-import { hydrateRoot } from "react-dom/client";
+import { RemixBrowser } from '@remix-run/react';
+import { StrictMode, startTransition } from 'react';
+import { hydrateRoot } from 'react-dom/client';
+import { cacheAssets } from 'remix-utils/cache-assets';
+
+cacheAssets({ cacheName: 'assets', buildPath: '/build/' })
+	.then(() => {
+		console.log('Assets cached');
+	})
+	.catch(error => {
+		console.error('Error caching assets:', error);
+	});
 
 startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
+	hydrateRoot(
+		document,
+		<StrictMode>
+			<RemixBrowser />
+		</StrictMode>,
+	);
 });
